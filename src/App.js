259d05236a78f0ec7e67d33705a02c88185c423a
Pranav Magare <vickymagare1996@gmail.com>
2022-11-07 09:45:36 +0530

    add

--- src/App.js
+++ src/App.js
@@ -6,25 +6,25 @@ function App() {
   const Data = [
     {
       id: 1,
-      question: "What is the React?",
+      question: "What is React?",
       answer:
         " React is a free and open-source front-end JavaScript library for building user interfaces based on UI components. It is maintained by Meta and a community of individual developers and companies.",
     },
     {
       id: 2,
-      question: "What is the Anguler?",
+      question: "What is Anguler?",
       answer:
         "Angular is a TypeScript-based free and open-source web application framework lead by the Angular Team at Google and by a community of individuals and corporations. Angular is a complete rewrite from the same team that built AngularJS",
     },
     {
       id: 3,
-      question: "What is the TypeScript?",
+      question: "What is TypeScript?",
       answer:
         " TypeScript is a free and open source programming language developed and maintained by Microsoft. It is a strict syntactical superset of JavaScript and adds optional static typing to the language. It is designed for the development of large applications and transpiles to JavaScript.",
     },
     {
       id: 4,
-      question: "What is the Javascript?",
+      question: "What is Javascript?",
       answer:
         " JavaScript, often abbreviated as JS, is a programming language that is one of the core technologies of the World Wide Web, alongside HTML and CSS. As of 2022, 98% of websites use JavaScript on the client side for webpage behavior, often incorporating third-party libraries. ",
     },
@@ -33,7 +33,6 @@ function App() {
   const [active, setActive] = useState(null);
 
   const toggleHandler = (id) => {
-    console.log("ID", id);
     active === id ? setActive(null) : setActive(id);
   };
 
--- src/Components/AccordionWrapper.jsx
+++ src/Components/AccordionWrapper.jsx
@@ -1,32 +1,38 @@
 import React from "react";
+import PropTypes from "prop-types";
 import "./AccordionWrapper.css";
 
 const AccordionWrapper = ({ data, toggleHandler, active }) => {
   return (
-    <>
-      <div className="container">
-        <div className="accordionWrapper">
-          {data.map((item) => {
-            return (
-              <div className="itemlist" key={item.id}>
-                <div className="title" onClick={() => toggleHandler(item.id)}>
-                  <h2>{item.question}</h2>
-                  <span className="btnActive">
-                    {active === item.id ? "-" : "+"}
-                  </span>
-                </div>
-                <div
-                  className={active === item.id ? "content show" : "content"}
-                >
-                  {item.answer}
-                </div>
+    <div className="container">
+      <div className="accordionWrapper">
+        {data.map((item) => {
+          return (
+            <div className="itemlist" key={item.id}>
+              <div className="title" onClick={() => toggleHandler(item.id)}>
+                <h2>{item.question}</h2>
+                <span className="btnActive">
+                  {active === item.id ? "-" : "+"}
+                </span>
               </div>
-            );
-          })}
-        </div>
+              <div className={active === item.id ? "content show" : "content"}>
+                {item.answer}
+              </div>
+            </div>
+          );
+        })}
       </div>
-    </>
+    </div>
   );
+};
+
+AccordionWrapper.defaultProps = {
+  active: null,
+};
+
+AccordionWrapper.propTypes = {
+  toggleHandler: PropTypes.func,
+  active: PropTypes.number,
 };
 
 export default AccordionWrapper;
